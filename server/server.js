9ff738c46b10fbd18ccdc5901017912df11a9a09
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-12 00:08:32 +0100

    finished server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,20 @@
 import devBundle from "./devBundle"; // comment out for production
 const app = express();
 devBundle.compile(app); // commment out for production
+
+import path from "path";
+const CURRENT_WORKING_DIR = process.cwd();
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+import template from "./../template";
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", port);
+});
