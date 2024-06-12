f61cd0d9185bb1024ae174ea80bee728a96a2c4b
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-12 17:41:46 +0100

    imported config variables in server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-import express from "express";
+import config from "./../config/config";
+import app from "./express";
 import devBundle from "./devBundle"; // comment out for production
-const app = express();
+import path from "path";
+import template from "./../template";
+import { MongoClient } from "mongodb";
+
+const app = app();
 devBundle.compile(app); // commment out for production
 
-import path from "path";
 const CURRENT_WORKING_DIR = process.cwd();
 app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
 
-import template from "./../template";
 app.get("/", (req, res) => {
   res.status(200).send(template());
 });
@@ -20,10 +23,16 @@ app.listen(port, function onStart(err) {
   console.info("Server started on port %s.", port);
 });
 
-import { MongoClient } from "mongodb";
 const url =
   process.env.MONGODB_URI || "mongodb://localhost:27017/mernSimpleSetup";
 MongoClient.connect(url, (err, db) => {
   console.log("Connected successfully to mongodb server");
   db.close();
+});
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", config.port);
 });
