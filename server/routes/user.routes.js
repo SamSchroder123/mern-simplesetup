67e99144234ddd86b4c2035bd688644149e7c884
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-16 00:21:25 +0100

    added API endpoint to update user by id

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,3 +2,4 @@ router.route("/api/users").post(userCtrl.create);
 router.route("/api/users").get(userCtrl.list);
 router.param("userId", userCtrl.userByID);
 router.route("/api/users/:userId").get(userCtrl.read);
+router.route("/api/users/:userId").put(userCtrl.update);
