34920d1db6a9eb1bc953a1744689fa87ec2fb626
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-16 00:16:07 +0100

    added API endpoint to read a single user's data

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,3 +1,4 @@
 router.route("/api/users").post(userCtrl.create);
 router.route("/api/users").get(userCtrl.list);
 router.param("userId", userCtrl.userByID);
+router.route("/api/users/:userId").get(userCtrl.read);
