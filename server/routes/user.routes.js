d735affd7bf5263298abf35ed37013e9be02c80f
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-16 00:25:19 +0100

    added API endpoint to delete user by id

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,3 +3,4 @@ router.route("/api/users").get(userCtrl.list);
 router.param("userId", userCtrl.userByID);
 router.route("/api/users/:userId").get(userCtrl.read);
 router.route("/api/users/:userId").put(userCtrl.update);
+router.route("/api/users/:userId").delete(userCtrl.remove);
