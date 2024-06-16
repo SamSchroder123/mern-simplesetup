0801e40b7e9debab6f775d3ea59340c2995910d1
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-16 10:14:45 +0100

    added error catching code for authorization errors in express.js

--- server/express.js
+++ server/express.js
@@ -19,6 +19,14 @@ const configureExpressApp = () => {
   app.use(cors());
   app.use("/", userRoutes);
   app.use("/", authRoutes);
+  app.use((err, req, res, next) => {
+    if (err.name === "UnauthorizedError") {
+      res.status(401).json({ error: err.name + ": " + err.message });
+    } else if (err) {
+      res.status(400).json({ error: err.name + ": " + err.message });
+      console.log(err);
+    }
+  });
 
   app.get("/", (req, res) => {
     res.status(200).send(Template());
