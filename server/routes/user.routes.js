502248ab8a41137c2d322049ffa550dbab1a75a9
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-16 08:41:05 +0100

    added route for userId

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,14 @@
+import userCtrl from "../controllers/user.controller";
+import authCtrl from "../controllers/auth.controller";
+
 router.route("/api/users").post(userCtrl.create);
 router.route("/api/users").get(userCtrl.list);
 router.param("userId", userCtrl.userByID);
 router.route("/api/users/:userId").get(userCtrl.read);
 router.route("/api/users/:userId").put(userCtrl.update);
 router.route("/api/users/:userId").delete(userCtrl.remove);
+router
+  .route("/api/users/:userId")
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
